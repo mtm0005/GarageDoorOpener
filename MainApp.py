092f211b7a500debaf78d69ee2fa3af7ad0a5e58
Tad McFall <mtm0005@gmail.com>
2018-04-29 14:07:59 -0500

    Added MainApp.py for raspi

---
+++ MainApp.py
@@ -0,0 +1,140 @@
+#! /usr/local/bin/python3
+
+import email
+import getpass
+import imaplib
+import RPi.GPIO as GPIO
+import spidev
+import sys
+import time
+
+from lib_nrf24 import NRF24
+
+def get_mailbox(email_address, server='imap.gmail.com'):
+    password = getpass.getpass()
+
+    mail = imaplib.IMAP4_SSL(server)
+    mail.login(email_address, password)
+    mail.select('inbox')
+    return mail
+
+def get_radio():
+    GPIO.setmode(GPIO.BCM)
+    pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(0, 17)
+    # TO-DO: Reset radio (just the line below) if program gets killed.
+    radio.stopListening()
+
+    radio.setPayloadSize(32)
+    radio.setChannel(0x76)
+    radio.setDataRate(NRF24.BR_250KBPS)
+    #radio.setDataRate(NRF24.BR_1MBPS)
+    radio.setPALevel(NRF24.PA_MIN)
+
+    radio.setAutoAck(True)
+    radio.enableDynamicPayloads()
+    radio.enableAckPayload()
+
+    radio.openWritingPipe(pipes[0])
+    radio.openReadingPipe(1, pipes[1])
+    radio.printDetails()
+    return radio
+
+# Doesn't work if you send messages from computer. Use phone instead.
+def get_commands(mailbox):
+    search_criteria = 'UNSEEN FROM mtm0005@gmail.com SUBJECT GarageDoor'
+    status, data = mailbox.search(None, search_criteria)
+    mail_ids = data[0]
+
+    email_messages = []
+    if status == 'OK':
+        for mail_id in mail_ids.split():
+            print('mail_id: {}'.format(mail_id))
+            print('type(mail_id): {}'.format(type(mail_id)))
+            status, raw_email_data = mailbox.fetch(mail_id, 'RFC822')
+            email_messages.append(email.message_from_bytes(raw_email_data[0][1]))
+    else:
+        sys.exit('Error trying to read email')
+
+    print('Read {} emails'.format(len(email_messages)))
+    commands = []
+    for msg in email_messages:
+        print('payload: {}'.format(msg.get_payload()))
+        print('type(payload): {}'.format(type(msg.get_payload())))
+        commands.append(msg.get_payload())
+    
+    return commands
+
+def build_command_from_str(command):
+    cmd_msg = list(command)
+    while len(cmd_msg) < 32:
+        cmd_msg.append(0)
+    return cmd_msg
+
+def send_arduino_command(radio, command, timeout=5.0):
+    print('Sending command: {}'.format(command))
+    command = build_command_from_str(command)
+    start_time = time.time()
+    response = ''
+    while start_time + timeout >= time.time():
+        radio.stopListening()
+        radio.write(command)
+        radio.startListening()
+        while not radio.available():
+            time.sleep(0.002)
+            if start_time + timeout/10 >= time.time():
+                break
+
+        # Read a message if one is available.
+        if radio.available():
+            raw_response = []
+            radio.read(raw_response, radio.getDynamicPayloadSize())
+            print('Received: {}'.format(raw_response))
+            print('Translating the raw_response into unicode characters')
+            for i in raw_response:
+                # Decode into standard unicode set
+                if i >= 32 and i <= 126:
+                    response += chr(i)
+            print('Out received message decodes to : {}'.format(response))
+            break
+
+    if not response:
+        print('Command, {}, timed out.')
+
+    radio.stopListening()
+
+    return response
+
+def check_door_status(mailbox, radio):
+    response = send_arduino_command(radio, 'CheckDoorStatus')
+
+def open_door(mailbox, radio):
+    response = send_arduino_command(radio, 'OpenDoor')
+
+def close_door(mailbox, radio):
+    response = send_arduino_command(radio, 'CloseDoor')
+
+def main():
+    mailbox = get_mailbox('rasmcfall@gmail.com')
+    radio = get_radio()
+
+    while True:
+        commands = get_commands(mailbox)
+        print('Received {} commands.'.format(len(commands)))
+
+        # Parse commands and execute commands if needed
+        if commands:
+            for command in commands:
+                if command == 'CheckDoorStatus':
+                    check_door_status(mailbox, radio)
+                elif command == 'OpenDoor':
+                    open_door(mailbox, radio)
+                elif command == 'CloseDoor':
+                    close_door(mailbox, radio)
+            
+        # Check arduino
+        time.sleep(10)
+
+main()
