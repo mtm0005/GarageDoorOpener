fe198e3f94007a0e16b215e0f29daf865045b884
Tad McFall <mtm0005@gmail.com>
2018-05-07 21:19:13 -0500

    Master.ino was cleaned, command structer built, messages parsed, fixed syntax errors

--- ArduinoMaster.ino
+++ ArduinoMaster.ino
@@ -1,6 +1,14 @@
 // Include libraries
+#ifdef ARDUINO
 #include<SPI.h>
 #include<RF24.h>
+#else
+#include "Master.hpp"
+#include "RF24_emulator.hpp"
+#include "ArduinoBuiltins.hpp"
+#include <vector>
+#include <sstream>
+#endif
 
 // Define pins
 const int trigPin = 2;
@@ -14,26 +22,41 @@ int garageDoorThreshold = 75;   // Garage door status threshold
                                 // Above this value represents a close door
                                 // Below this value represents an open door
 int garageDelay = 15;           // Amount of time it takes for the door to close/open
-int timeout = 3;                // Times to try garageDoorRemote
+const int ITERATIONATTEMPTS = 3;                // Times to try garageDoorRemote
+const String OPENDOOR = "openDoor";
+const String CLOSEDOOR = "closeDoor";
+const String CHECKSTATUS = "checkStatus";
+const String SUCCESS = "success";
+const String FAILURE = "failure";
+const String ACK = "ack";
+
+struct Command {
+    String name;
+    String data;
+    int ID;
+};
 
 
 // --------------------- Function Definitions ------------------------
 
-float calculateAverage(int array[], int arrayLength){
+float calculateAverage(int array[], int arrayLength) {
     // Calculate the average of a given array
     float sum;
-    for (int i=0; i<arrayLength; i++){
-    sum = sum + array[i];
+    for (int i=0; i<arrayLength; i++) {
+        sum = sum + array[i];
+    }
+
+    return sum/arrayLength;
 }
 
-float calculateDistance(){
+float calculateDistance() {
     // Clear the trigPin
     digitalWrite(trigPin, LOW);
     delay(2);
 
     int distance[sampleCount];
-    long duration
-    for (int i=0; i<sampleCount; i++){
+    long duration;
+    for (int i=0; i<sampleCount; i++) {
         // Build an array of sensor measurements
 
         // Sets the trigPin on HIGH state for 10 micro seconds
@@ -46,18 +69,18 @@ float calculateDistance(){
         
         // Calculating the distance (cm)
         distance[i] = duration*0.034/2;
-  }
+    }
 
   return calculateAverage(distance, sampleCount);
 }
 
-char isGarageDoorClosed(){
+char isGarageDoorClosed() {
     // Check the current status of the grage door
     // true indicates a closed door
 
     float average = calculateDistance();
     
-    if (average > garageDoorThreshold){
+    if (average > garageDoorThreshold) {
         // A distance larger than the threshold signifies a clsoed door
         return true;
     }
@@ -66,7 +89,7 @@ char isGarageDoorClosed(){
     return false;  
 }
 
-void garageDoorRemote(){
+void garageDoorRemote() {
     // Execute garage door command
     digitalWrite(garageDoorOpener, HIGH);
     delay(250);
@@ -76,40 +99,116 @@ void garageDoorRemote(){
     // Note that this does not signify a successful open/close
 }
 
-bool garageDoorCommand(char desiredStatus){
+bool garageDoorCommand(String desiredStatus) {
     // returns true if the garageDoorCommand was executed successfully
-    for (int i=0; i<timeout; i++){
+    for (int i=0; i<ITERATIONATTEMPTS; i++){
         garageDoorRemote();
         
         delay(garageDelay);
 
-        if (desiredStatus == "open" && !isGarageDoorClosed()){
+        if (desiredStatus == OPENDOOR && !isGarageDoorClosed()) {
             return true;
         }
-        else if (desiredStatus == "closed" && isGarageDoorClosed()){
+        else if (desiredStatus == CLOSEDOOR && isGarageDoorClosed()) {
             return true;
         }
-        // Should be put another delay here? 
-        // What are the circumstances for this?
     }
 
     return false;
 }
 
-char readMessage(){
+std::vector<std::string> split(const std::string& s, char delimiter) {
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter)) {
+      tokens.push_back(token);
+   }
+
+   return tokens;
+}
+
+struct Command readMessage() {
+    char receivedMessage[32] = {0};
     radio.stopListening();
     radio.read(receivedMessage, sizeof(receivedMessage));
     
-    return stringMessage(receivedMessage);
+    String stringMessage(receivedMessage);
+
+    std::vector<std::string> command_vector = split(stringMessage, ',');
+    Command command;
+    command.name = command_vector[0];
+    command.ID = std::stoi(command_vector[1]);
+    if (command_vector.size() > 2) {
+        command.data = command_vector[2];
+    }
+
+    return command;
 }
 
-void sendMessage(char command, int ID, char message){
-    //send message
+void sendMessage(String command, int ID, String message) {
+    String response = command + ", " + std::to_string(ID) + ", " + message;
+    radio.write(response.c_str(), sizeof(response.c_str()));
+}
+
+
+void handleCommand(Command command) {
+    sendMessage(command.name, command.ID, ACK);
+    String result;
+    if (command.name == OPENDOOR) {
+        // openDoor command received
+        if (isGarageDoorClosed()) {
+            // Open door
+            if (garageDoorCommand(command.name)) {
+                result = SUCCESS;
+            }
+            else {
+                result = FAILURE;
+            }
+        }
+        else {
+            // Door is already open
+            result = SUCCESS;
+        }
+    }
+    else if (command.name == CLOSEDOOR) {
+        // closeDoor command received
+        if (!isGarageDoorClosed()) {
+            // Close door
+            if (garageDoorCommand(command.name)) {
+                result = SUCCESS;
+            }
+            else {
+                result = FAILURE;
+            }
+        }
+        else {
+            // Door is already closed
+            result = SUCCESS;
+        }
+    }
+    else if (command.name == CHECKSTATUS) {
+        // checkDoorStatus command received
+        if (isGarageDoorClosed()) {
+            // Garage door is closed
+            result = "closed";
+        }
+        else {
+            // Garage door is open
+            result = "open";
+        }
+    }
+    else {
+        // Invalid command received
+        result = "unknown message";
+    }
+
+    sendMessage(command.name, command.ID, result);
 }
 
 // ----------------------- Setup ----------------------------
 
-void setup(){
+void setup() {
     pinMode(trigPin, OUTPUT); // Set the trigPin as an output
     pinMode(garageDoorOpener, OUTPUT); // Set the garageDoorOpener pin as an output
     pinMode(echoPin, INPUT); // Setsthe echoPin as an input
@@ -127,54 +226,11 @@ void setup(){
     radio.powerUp();
 }
 
-void loop(){
+void loop() {
+    delay(100);
     radio.startListening();
 
-    char receivedMessage[32] = {0};
-    if (radio.available()){
-
-        sendMessage(receivedMessage(), 999, "ack");
-        if (receivedMessage() == "openDoor"){
-            // openDoor command received
-            if (isGarageDoorClosed()){
-                // open door
-                if (garageDoorCommand("open")){
-                    sendMessage("openDoor", 999, "open");
-                }
-            }
-            else{
-                // Door is already open
-                sendMessage("openDoor", 999, "open");
-            }
-        }
-        else if (receivedMessage() == closeDoor){
-            // closeDoor command received
-            if (!isGarageDoorClosed()){
-                // close door
-                if (garageDoorCommand("closed")){
-                    sendMessage("closeDoor", 999, "closed");
-                }
-            }
-            else{
-                // door is already closed
-                sendMessage("closeDoor", 999, "closed");
-            }
-        }
-        else if (receivedMessage() == "checkDoorStatus"){
-            // checkDoorStatus command received
-            if (isGarageDoorClosed()){
-                // Garage door is closed
-                sendMessage("checkDoorStatus", 999, "closed");
-            }
-            else{
-                // Garage door is open
-                sendMessage("checkDoorStatus", 999, "open");
-            }
-        }
-        else{
-            // Invalid command received
-            sendMessage("Unknown", 999, "Unknown Message");
-        }
+    if (radio.available()) {
+        handleCommand(readMessage());
     }
-    delay(100);
 }
--- Master.ino
+++ Master.ino
@@ -6,201 +6,271 @@
 #include "Master.hpp"
 #include "RF24_emulator.hpp"
 #include "ArduinoBuiltins.hpp"
+#include <vector>
+#include <sstream>
 #endif
 
-// Define pin locations
+// Define pins
 const int trigPin = 2;
 const int echoPin = 3;
 const int garageDoorOpener = 5;
 RF24 radio(7, 8);
 
-// Define variables)(ation;
-long duration;
-int sampleCount = 10;
-int garageDoorThreshold = 75;
+// Define universal variables
+int sampleCount = 10; // Number of samples used to check door status
+int garageDoorThreshold = 75;   // Garage door status threshold
+                                // Above this value represents a close door
+                                // Below this value represents an open door
+int garageDelay = 15;           // Amount of time it takes for the door to close/open
+const int ITERATIONATTEMPTS = 3;                // Times to try toggleGarageDoor
+const String OPENDOOR = "openDoor";
+const String CLOSEDOOR = "closeDoor";
+const String CHECKSTATUS = "checkStatus";
+const String SUCCESS = "success";
+const String FAILURE = "failure";
+const String ACK = "ack";
 
-// -------------------- Function Definitions ---------------------------
-// Calculate average
-float calculateAverage(int array[], int arrayLength){
-  float sum;
-  for (int i=0; i<arrayLength; i++){
-    sum = sum + array[i];
-  }
+struct Command {
+    String name;
+    String value;
+    int ID;
+};
 
-  return sum/arrayLength;
+
+// --------------------- Function Definitions ------------------------
+
+float calculateAverage(int array[], int arrayLength) {
+    // Calculate the average of a given array
+    float sum;
+    for (int i=0; i<arrayLength; i++) {
+        sum = sum + array[i];
+    }
+
+    return sum/arrayLength;
 }
 
-float calculate_distance() {
-  // Clear the trigPin
-  digitalWrite(trigPin, LOW);
-  delay(2);
-
-  // Build array of distances
-  int distance[sampleCount];
-  for (int i=0; i<sampleCount; i++){
-    
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
+float calculateDistance() {
+    // Clear the trigPin
     digitalWrite(trigPin, LOW);
-    
-    // Read the echoPin, return the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    
-    // Calculating the distance (cm)
-    distance[i] = duration*0.034/2;
-  }
+    delay(2);
+
+    int distance[sampleCount];
+    long duration;
+    for (int i=0; i<sampleCount; i++) {
+        // Build an array of sensor measurements
+
+        // Sets the trigPin on HIGH state for 10 micro seconds
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+        
+        // Read the echoPin, return the sound wave travel time in microseconds
+        duration = pulseIn(echoPin, HIGH);
+        
+        // Calculating the distance (cm)
+        distance[i] = duration*0.034/2;
+    }
 
   return calculateAverage(distance, sampleCount);
 }
 
-// Determine garage door status
-// Returns true if garage door is closed
-bool isGarageDoorClosed(){
-  Serial.println("Entering isGarageDoorClosed");
+char isGarageDoorClosed() {
+    // Check the current status of the grage door
+    // true indicates a closed door
 
-  float average = calculate_distance();
-  
-  Serial.print("Average distance: ");
-  Serial.println(average);
-  if (average > garageDoorThreshold){
-    Serial.println("Average distance is above threshold");
-    // Return true if garage door is closed
-    return false;
-  }
+    float average = calculateDistance();
+    
+    if (average > garageDoorThreshold) {
+        // A distance larger than the threshold signifies a clsoed door
+        Serial.println("Garage door determined to be close");
+        return true;
+    }
 
-  Serial.println("Average distance is below threshold");
-  return true;
-  
+    // Otherwise, the door is open
+    Serial.println("Garage door determined to be open");
+    return false;  
 }
 
-// Toggles the status of the garage door
-bool toggleGarageDoor(){
-  // Execute garage door command
-  digitalWrite(garageDoorOpener, HIGH);
-  delay(250);
-  digitalWrite(garageDoorOpener, LOW);
-  // Return true if command was executed properly
-  return true;
-}
-
-// --------------------------- Main ---------------------------
-
-void setup(){
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode(garageDoorOpener, OUTPUT);
-  Serial.begin(9600); // Starts the serial communication
-
-  while(!Serial);
-  Serial.begin(9600);
-
-  radio.begin();
-  radio.stopListening();
-  radio.setPALevel(RF24_PA_MIN);
-  radio.setChannel(0x76);
-  radio.openWritingPipe(0xF0F0F0F0E1LL);
-  const uint64_t pipe = (0xE8E8F0F0E1LL);
-  radio.openReadingPipe(1, pipe);
-
-  radio.enableDynamicPayloads();
-  radio.setDataRate(RF24_250KBPS);
-  radio.powerUp();
-
-  // LED test
-  for (int i=0; i<4; i++){
+void toggleGarageDoor() {
+    // Execute garage door command
     digitalWrite(garageDoorOpener, HIGH);
     delay(250);
     digitalWrite(garageDoorOpener, LOW);
-    delay(250);
-  }
+
+    // Return true if command was executed properly
+    // Note that this does not signify a successful open/close
 }
 
-void loop(){
-  radio.startListening();
+bool garageDoorCommand(String desiredStatus) {
+    // returns true if the garageDoorCommand was executed successfully
+    for (int i=0; i<ITERATIONATTEMPTS; i++){
+        Serial.print("toggleGarageDoor: iteration ");
+        Serial.println(i+1);
+        toggleGarageDoor();
+        
+        delay(garageDelay);
 
-  Serial.println("Listening...");
-  
-  char receivedMessage[32] = {0};
-  if(radio.available()){
+        if (desiredStatus == OPENDOOR && !isGarageDoorClosed()) {
+            Serial.println("garageDoorCommand succeeded");
+            return true;
+        }
+        else if (desiredStatus == CLOSEDOOR && isGarageDoorClosed()) {
+            Serial.println("garageDoorCommand succeeded");
+            return true;
+        }
+    }
+    Serial.println("garageDoorCommand failed");
+    return false;
+}
+
+std::vector<std::string> split(const std::string& s, char delimiter) {
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter)) {
+      tokens.push_back(token);
+   }
+
+   return tokens;
+}
+
+struct Command readMessage() {
+    Serial.println("Reading message");
+    char receivedMessage[32] = {0};
     radio.stopListening();
     radio.read(receivedMessage, sizeof(receivedMessage));
     
-    Serial.print("Message Received: ");
-    Serial.println(radio.command);
-
     String stringMessage(receivedMessage);
 
-    if(stringMessage == "OpenDoor"){
-      Serial.println("OpenDoor command received");
-      const char text[] = "OpenDoor command Received";
-      radio.write(text, sizeof(text));
+    Serial.print("Recieved message: ");
+    Serial.println(stringMessage);
 
-      if (isGarageDoorClosed()){
-        bool toggleResult = toggleGarageDoor();
-        Serial.println("Garage door determined to be close");
-        Serial.println("toggleGarageDoor function called");
-        delay(5000); // Delay time for garage door to open/close
-        if (!isGarageDoorClosed()){
-          Serial.println("Garage door opened successfully");
-          //const char text[] = "Garage dooor opened successfully";
-          //radio.write(text, sizeof(text));
-        }
-        else{
-          Serial.println("Command Failed");
-          //const char text[] = "Command Failed";
-          //radio.write(text, sizeof(text));
-        }
-      }
-      else{
-        Serial.println("Garage door is already open");
-      }
-    }
-    else if(stringMessage == "CloseDoor"){
-      Serial.println("CloseDoor command received");
-      const char text[] = "CloseDoor command received";
-      radio.write(text, sizeof(text));
-      
-      if (!isGarageDoorClosed()){
-        bool toggleResult = toggleGarageDoor();
-        delay(5000);
-        if (isGarageDoorClosed()){
-          Serial.println("Garage door closed successfully");
-          //const char text[] = "Garage dooor closed successfully";
-          //radio.write(text, sizeof(text));
-        }
-        else{
-          Serial.println("Command Failed");
-          //const char text[] = "Command Failed";
-          //radio.write(text, sizeof(text));
-        }
-      }
-      else{
-        Serial.println("Garage door is already closed");
-      }
-    }
-    else if (stringMessage == "CheckDoorStatus"){
-      Serial.println("CheckDoorStatus command received");
-      const char text[] = "CheckDoorStatus command received";
-      radio.write(text, sizeof(text));
+    std::vector<std::string> commandVector = split(stringMessage, ',');
 
-      if (isGarageDoorClosed()){
-        Serial.println("Garage door is currently closed");
-        //const char text[] = "Closed";
-        //radio.write(text, sizeof(text));
-      }
-      else{
-        Serial.println("Garage door is currently open");
-        //const char text[] = "Open";
-        //radio.write(text, sizeof(text));
-      }
+    Serial.println("message was split");
+    Command command;
+
+    // Reject messages that aren't in the proper form.
+    if (commandVector.size() > 3 || commandVector.size() < 2) {
+        command.name = "INVALID";
+        command.ID = -1;
+        return command;
     }
-    else{
-      Serial.println("Unknown command received");
-      //const char text[] = "Unknown command received";
-      //radio.write(text, sizeof(text));
+
+    command.name = commandVector[0];
+    command.ID = std::stoi(commandVector[1]);
+    if (commandVector.size() > 2) {
+        command.value = commandVector[2];
     }
-  delay(1000);
-  }
-  delay(200);
+
+    Serial.println("Command: ");
+    Serial.print("    name: ");
+    Serial.println(command.name);
+    Serial.print("    ID: ");
+    Serial.println(command.ID);
+    Serial.print("    value: ");
+    Serial.println(command.value);
+
+    return command;
+}
+
+void sendMessage(String command, int ID, String message) {
+    Serial.println("Sending message...");
+    String response = command + ", " + std::to_string(ID) + ", " + message;
+    radio.write(response.c_str(), sizeof(response.c_str()));
+    Serial.print("response: ");
+    Serial.println(response);
+}
+
+
+void handleCommand(Command command) {
+    Serial.println("Handling message");
+    sendMessage(command.name, command.ID, ACK);
+    String result;
+    if (command.name == OPENDOOR) {
+        // openDoor command received
+        if (isGarageDoorClosed()) {
+            // Open door
+            if (garageDoorCommand(command.name)) {
+                result = SUCCESS;
+            }
+            else {
+                result = FAILURE;
+            }
+        }
+        else {
+            // Door is already open
+            Serial.println("Door was already open");
+            result = SUCCESS;
+        }
+    }
+    else if (command.name == CLOSEDOOR) {
+        // closeDoor command received
+        if (!isGarageDoorClosed()) {
+            // Close door
+            if (garageDoorCommand(command.name)) {
+                result = SUCCESS;
+            }
+            else {
+                result = FAILURE;
+            }
+        }
+        else {
+            // Door is already closed
+            Serial.println("Door was already closed");
+            result = SUCCESS;
+        }
+    }
+    else if (command.name == CHECKSTATUS) {
+        // checkDoorStatus command received
+        Serial.println("checkStatus command received");
+        if (isGarageDoorClosed()) {
+            // Garage door is closed
+            result = "closed";
+        }
+        else {
+            // Garage door is open
+            result = "open";
+        }
+    }
+    else {
+        // Invalid command received
+        Serial.println("Unknown message received");
+        result = "unknown message";
+    }
+
+    sendMessage(command.name, command.ID, result);
+}
+
+// ----------------------- Setup ----------------------------
+
+void setup() {
+    pinMode(trigPin, OUTPUT); // Set the trigPin as an output
+    pinMode(garageDoorOpener, OUTPUT); // Set the garageDoorOpener pin as an output
+    pinMode(echoPin, INPUT); // Setsthe echoPin as an input
+
+    Serial.begin(9600);
+    while(!Serial);
+
+    radio.begin();
+    radio.stopListening();
+    radio.setPALevel(RF24_PA_MIN);
+    radio.setChannel(0x76);
+    radio.openWritingPipe(0xF0F0F0F0E1LL);
+    const uint64_t pipe = (0xE8E8F0F0E1LL);
+    radio.openReadingPipe(1, pipe);
+
+    radio.enableDynamicPayloads();
+    radio.setDataRate(RF24_250KBPS);
+    radio.powerUp();
+}
+
+void loop() {
+    delay(100);
+    radio.startListening();
+
+    if (radio.available()) {
+        Serial.println("Message available");
+        handleCommand(readMessage());
+    }
 }
