dfa2c03cfceac9bba181a17975602d680022d980
Tad McFall <mtm0005@gmail.com>
2018-06-17 00:28:42 -0500

    Updated to run on linux

--- cronjob.sh
+++ cronjob.sh
@@ -4,7 +4,7 @@
 # Then input: */5 * * * * ~/git/GarageDoorOpener/cronjob.sh
 # This will run the cronjob.sh script every 5 minutes.
 
-process=$(ps | grep "raspi_master.py" | grep -v grep | awk '{print $1}')
+process=$(ps -ef | grep "raspi_master.py" | grep -v grep | awk '{print $1}')
 
 # If the process is NOT running...
 if [[ $process = "" ]]
