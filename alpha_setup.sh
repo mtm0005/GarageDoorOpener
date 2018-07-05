30ac888dcb643c29f7e632ab447c63dbe5de5900
Tyler Sherrod <tylersherrod@Tylers-MacBook-Pro.local>
2018-07-04 20:44:39 -0500

    Created alpha_setup.sh to aid in RasPi setup for alpha testing.
    Script checks-out alpha branch,
    installs necessary modules,
    modifies crontab

--- alpha_setup.sh
+++ alpha_setup.sh
@@ -1,10 +1,25 @@
 # Connect to wi-fi
 # git clone repository
-
-cd ~/home/git/GarageDoorController
+# Run this script
 
 # checkout alpha branch
 git checkout alpha
 git pull
 
+# Install required modules
+list_of_modules="datetime
+Enum
+firebase
+GoogleAuth
+os
+pyfcm
+RPi.GPIO
+subprocess"
+
+for module in $list_of_modules
+do
+    sudo pip3 install $module
+done
+
 # modify crontab
+echo '*/5 * * * * ~/git/GarageDoorOpener/cronjob.sh' | crontab -
